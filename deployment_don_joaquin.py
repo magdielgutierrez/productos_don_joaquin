951932ac5f4ccdd20a9f48111ab631e0f396fe4c
Magdiel Gutiérrez <mgutierreztwp@hotmail.com>
2024-02-19 21:13:18 -0500

    fix: name flows

--- deployment_don_joaquin.py
+++ deployment_don_joaquin.py
@@ -1,10 +1,12 @@
 from prefect import flow
 
+
 if __name__ == "__main__":
     flow.from_source(
-        entrypoint="flows_read_inventory.py:flows_read_inventory",
+        source='https://github.com/magdielgutierrez/productos_don_joaquin.git',
+        entrypoint="flows_read_inventary.py:flows_read_inventory",
     ).deploy(
-        name="flows_read_inventary",
+        name="productos_don_joaquin_",
         work_pool_name="productos_don_joaquin_produccion",
         cron="0 13 * * *",
     )
