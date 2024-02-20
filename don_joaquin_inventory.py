8b845b6fdbaf0c5dd6b747686a59845272ac8d09
magdielgutierrez.local <mgutierreztwp@hotmail.com>
2024-02-20 07:43:15 -0500

    refactor: change name files

---
+++ don_joaquin_inventory.py
@@ -0,0 +1,51 @@
+from prefect import flow, task
+from prefect.filesystems import SMB
+from io import StringIO
+import pandas as pd
+
+# smb_block = SMB.load("don-joaquin-inventario")
+# data=smb_block.read_path('/INVENTARIO PLANTA 2024/PLANILLA DE LECHE 2024.xlsx')
+
+# xls = pd.ExcelFile(data.decode('ISO-8859-1'))
+# sheets = xls.sheet_names
+
+# results = {}
+# for sheet in sheets:
+#     results[sheet] = xls.parse(sheet)
+    
+# xls.close()
+    
+
+@task
+def get_file_inventory(smb_block):
+    
+    data=smb_block.read_path('/INVENTARIO PLANTA 2024/PLANILLA DE LECHE 2024.xlsx')
+
+    xls = pd.ExcelFile(data)
+    sheets = xls.sheet_names
+  
+    results = {}
+    for sheet in sheets:
+        results[sheet] = xls.parse(sheet)
+        
+    xls.close()
+    
+    return results,sheets
+
+
+@flow(log_prints=True)
+def flows_read_inventory():
+    smb_block = SMB.load("don-joaquin-inventario")
+    results, sheets=get_file_inventory(smb_block)
+
+    print('\n',sheets)
+
+if __name__ == "__main__":
+    flows_read_inventory.from_source(
+        source='https://github.com/magdielgutierrez/productos_don_joaquin.git',
+        entrypoint="don_joaquin_inventory.py:flows_read_inventory",
+    ).deploy(
+        name="don_joaquin_produccion",
+        work_pool_name="don-joaquin-lacteos",
+        cron="0 13 * * *",
+    )
