3e8adef713466eb18b8a4d39960b661e481f5fd4
magdielgutierrez.local <mgutierreztwp@hotmail.com>
2024-02-21 20:10:42 -0500

    feature: save data in database

--- don_joaquin_inventory.py
+++ don_joaquin_inventory.py
@@ -1,8 +1,13 @@
 from prefect import flow, task
 from prefect.filesystems import SMB
+from prefect.filesystems import LocalFileSystem
+from prefect_sqlalchemy import SqlAlchemyConnector
 from io import StringIO
 import pandas as pd
 import re
+import warnings
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 def _textDateComplete(txt_mes,txt_days,year):
     
@@ -138,23 +143,79 @@ def _formatSheets(rDataOne,key):
         
         return rDataOne
 
+
+@task
+def post_data_in_database(data):
+    
+    sql_create=""" CREATE TABLE IF NOT EXISTS don_joaquin_production (pdn_date TEXT,pdn_provider TEXT,
+                pdn_unit_price REAL,pdn_subsidy INTEGER,pdn_quantity INTEGER, pdn_amount_paid REAL,
+                pdn_amount_sub INTEGER,pdn_total_amount REAL,pdn_type TEXT,pdn_num_week INTEGER,
+                pdn_year INTEGER,pdn_month INTEGER,pdn_name_month TEXT);"""
+        
+    with SqlAlchemyConnector.load("cnxn-lacteos-don-joaquin-produccion") as database:
+        database.execute(sql_create.replace('\t','').replace('\n',''))
+        database.execute_many(
+        """INSERT INTO don_joaquin_production (pdn_date,pdn_provider,pdn_unit_price,pdn_subsidy,pdn_quantity,
+                                                pdn_amount_paid,pdn_amount_sub,pdn_total_amount,pdn_type,
+                                                pdn_num_week,pdn_year,pdn_month,pdn_name_month) 
+                                        VALUES (:pdn_date,:pdn_provider,:pdn_unit_price,:pdn_subsidy,:pdn_quantity,
+                                                :pdn_amount_paid,:pdn_amount_sub,:pdn_total_amount,:pdn_type,
+                                                :pdn_num_week,:pdn_year,:pdn_month,:pdn_name_month);""".replace('\n',''),
+            seq_of_parameters=data.to_dict("records"),
+        )
+    
+    
+
+
+       
+
+
 @flow(log_prints=True)
 def flows_read_inventory():
+    dfAllData = pd.DataFrame()
     smb_block = SMB.load("don-joaquin-inventario")
     results, sheets=get_file_inventory(smb_block)
-
-    print('\n')
-
+    
     for key, rawData in results.items():
         
-        print(f"\t Nombre de la Hoja que se esta extrayendo la data {key}")
+        print(f"Nombre de la Hoja que se esta extrayendo la data {key}")
                
         rawData= _formatSheets(rawData,key)
-        print(rawData)
-        smb_block.write_path(path='filename.txt', content=rawData.encode('utf-8'))
-        break
+                         
+        groupColumns=rawData.columns.values.tolist()
+        dfData = pd.melt(rawData, id_vars=['Proveedor','PrecioUnit','Subsidio'], value_vars=groupColumns[3:], var_name='Fecha', value_name='Cantidad')
+        dfData=dfData[['Fecha','Proveedor','PrecioUnit','Subsidio','Cantidad']]
+        dfData=dfData[dfData['Cantidad'] != 0]
+        dfData['Fecha_temp'] = pd.to_datetime(dfData['Fecha'], format='%d/%m/%Y')
+        dfData['pdn_amount_paid']= dfData['PrecioUnit'] * dfData['Cantidad']
+        dfData['pdn_amount_sub']= dfData['Subsidio'] * dfData['Cantidad']
+        dfData['pdn_total_amount']=dfData['pdn_amount_paid']+dfData['pdn_amount_sub']
+        dfData['pdn_type']= dfData.apply(lambda row:_clasificarProveedor(row[1],row[2]), axis=1)
+        
+        dfData['pdn_num_week'] = dfData['Fecha_temp'].dt.isocalendar().week
+        dfData['pdn_year'] = dfData['Fecha_temp'].dt.isocalendar().year
+        dfData['pdn_month'] = dfData['Fecha_temp'].dt.month
+        dfData['pdn_name_month'] = dfData['Fecha_temp'].dt.strftime('%b')
+        
+        dfAllData = pd.concat([dfAllData, dfData], ignore_index=True)
+        #break
+    
+    dfAllData=dfAllData.drop_duplicates()
+    dfAllData=dfAllData.rename(columns={"Fecha_temp": "pdn_date",
+                                        "PrecioUnit": "pdn_unit_price",
+                                        "Proveedor":"pdn_provider",
+                                        "Cantidad":"pdn_quantity",
+                                        "Subsidio":"pdn_subsidy"})
+
+    dfAllData['pdn_date']=dfAllData['pdn_date'].astype(str)
+    dfAllData=dfAllData.drop(['Fecha'], axis=1)
+    # print(dfAllData.sample(1).T)
+    # print(dfAllData.to_dict("records")[0])
+    post_data_in_database(dfAllData)
+    
 
 if __name__ == "__main__":
+    #flows_read_inventory()
     flows_read_inventory.from_source(
         source='https://github.com/magdielgutierrez/productos_don_joaquin.git',
         entrypoint="don_joaquin_inventory.py:flows_read_inventory",
